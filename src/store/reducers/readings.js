6522ab2fabb4a75827075bdd27bf2d9155a70800
Bryan Borgesano <BryanBorgesano97@gmail.com>
2021-03-01 15:36:28 -0500

    Added initialState and case for saving all readings

---
+++ src/store/reducers/readings.js
@@ -0,0 +1,23 @@
+import {GET_READINGS, LOADING_READINGS} from "../actions/readings";
+
+const initialState = {
+  readings: [{}],
+  loading: false,
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case GET_READINGS:
+      return {
+        ...state,
+        readings: action.payload,
+      };
+    case LOADING_READINGS:
+      return {
+        ...state,
+        loading: action.payload,
+      };
+    default:
+      return {...state};
+  }
+};
