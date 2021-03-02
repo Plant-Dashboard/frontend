8bdec05620cdb5cc70d6c5308d76e2e11984456f
Bryan Borgesano <BryanBorgesano97@gmail.com>
2021-03-02 13:02:42 -0500

    Added refresh button to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,10 @@ import {makeStyles} from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import SyncIcon from "@material-ui/icons/Sync";
+import IconButton from "@material-ui/core/IconButton";
+import {useDispatch, useSelector} from "react-redux";
+import * as readingActions from "../store/actions/readings";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -17,6 +21,7 @@ const useStyles = makeStyles(theme => ({
 
 const Header = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
 
   return (
     <div className={classes.root}>
@@ -25,6 +30,16 @@ const Header = () => {
           <Typography variant="h6" className={classes.title}>
             Plant Dashboard
           </Typography>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+              onClick={async () => await dispatch(readingActions.getReadingsForToday())}>
+              <SyncIcon />
+            </IconButton>
+          </Toolbar>
         </Toolbar>
       </AppBar>
     </div>
