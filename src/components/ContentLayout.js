9dacac28fdedbe0095280fb83a57f9a0a954b8ab
Bryan Borgesano <BryanBorgesano97@gmail.com>
2021-04-18 10:18:44 -0400

    Removed camera and added filtered data

--- src/components/ContentLayout.js
+++ src/components/ContentLayout.js
@@ -3,9 +3,10 @@ import {makeStyles} from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import LiveData from "./LiveData";
 import HistoricalData from "./HistoricalData";
-import Camera from "./Camera";
 import Header from "../components/Header";
+import FilterData from "../components/FilterData";
 const useStyles = makeStyles(theme => ({
+  
   root: {
     flexGrow: 1,
     textAlign: "center",
@@ -25,17 +26,17 @@ const ContentLayout = props => {
   return (
     <div className={classes.root}>
       <Header />
-      <Grid container spacing={1} justify="center">
-        <Grid item lg={6} md={8}>
-          <Camera />
-        </Grid>
-        <Grid item lg={6} md={4} sm={12}>
+      <Grid container spacing={1} >
+        <Grid item lg={6} md={6} sm={12}>
           <LiveData />
+        </Grid> 
+        <Grid item lg={6} md={6} sm={12}>
+          <HistoricalData />
         </Grid>
       </Grid>
       <Grid container>
         <Grid item md={12} sm={12}>
-          <HistoricalData />
+          <FilterData />
         </Grid>
       </Grid>
     </div>
