400c1809860e0dfafe306d65831ac65578dda10d
Bryan Borgesano <BryanBorgesano97@gmail.com>
2021-04-19 09:24:17 -0400

    Created action to get filtered readings and uses pagination

---
+++ src/store/actions/filteredReadings.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import {errorResponse} from "../../util/errorResponse";
+import moment from "moment";
+export const GET_FILTERED_READINGS = "GET_FILTERED_READINGS";
+export const NO_RESULTS_FOUND = "NO_RESULTS_FOUND";
+
+export const getReadingWithParams = (type, params, page = 1) => {
+  return async dispatch => {
+    try {
+      const res = await axios.get(
+        `${process.env.REACT_APP_API_ENDPOINT}/api/v1/readings?${type}[gte]=${params[0]}&${type}[lte]=${params[1]}&page=${page}`
+      );
+
+      let {success, count, pagination} = res.data;
+    
+      if (success) {
+        if (count > 0) {
+          dispatch({type: "GET_FILTERED_READINGS", payload: res.data});
+        } else {
+          dispatch({type: "NO_RESULTS_FOUND", payload: true});
+        }
+      }
+    } catch (err) {
+      errorResponse(err);
+      dispatch({type: "LOADING_READINGS", payload: false});
+    }
+  };
+};
