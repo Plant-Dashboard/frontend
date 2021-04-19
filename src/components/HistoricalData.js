eb06f67d25922e2c9b5e293739c15e6948396156
Bryan Borgesano <BryanBorgesano97@gmail.com>
2021-04-19 09:23:40 -0400

    Now shows loading spinner. Updated colors to look better on dark theme

--- src/components/HistoricalData.js
+++ src/components/HistoricalData.js
@@ -4,6 +4,7 @@ import moment from "moment";
 import {LineChart, Line, CartesianGrid, XAxis, YAxis, Legend, Tooltip, ResponsiveContainer} from "recharts";
 import {makeStyles} from "@material-ui/core/styles";
 import {useSelector} from "react-redux";
+import Loader from "react-loader-spinner";
 
 const useStyles = makeStyles(theme => ({
   paper: {
@@ -17,7 +18,10 @@ const HistoricalData = () => {
   const readingState = useSelector(state => state.readings);
 
   if (readingState.loading) {
-    return <h1>Loading...</h1>;
+    return <Loader type="TailSpin" color="white" height={80} width={80}/>
+  }
+  if (readingState.readings[0] === undefined) {
+    return <h1>Error loading today's values</h1>;
   }
   return (
     <Paper className={classes.paper}>
@@ -26,13 +30,13 @@ const HistoricalData = () => {
       </h3>
       <ResponsiveContainer height={350} minWidth={200}>
         <LineChart data={readingState.readings} margin={{top: 5, right: 50, bottom: 25, left: 0}}>
-          <Line type="monotone" dataKey="temperature" stroke="#a81313" />
-          <Line type="monotone" dataKey="humidity" stroke="#134e91" />
+          <Line type="monotone" dataKey="temperature" stroke="#e8ca5d" />
+          <Line type="monotone" dataKey="humidity" stroke="#24adb5" />
           <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
           <XAxis dataKey="readingTime" tickFormatter={tickItem => moment(tickItem).format("LT")} />
           <YAxis />
           <Legend />
-          <Tooltip labelFormatter={label => moment(label).format("LLL")} />
+          <Tooltip labelFormatter={label => moment(label).format("LLL")}  contentStyle={{backgroundColor: '#303030'}} />
         </LineChart>
       </ResponsiveContainer>
     </Paper>
